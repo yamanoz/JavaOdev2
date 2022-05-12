2abd1862db31d48aa455a648f667141eb1019a3a
Özgür Yaman <yamanozgur6333@gmail.com>
2022-05-12 20:55:21 +0300

    Kullanıcı Adı ile Şifre giriş yapan program tasarlandı

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,47 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        String userName,password;
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Kullanici Adiniz :");
+        userName=scan.nextLine();
+        System.out.print("Sifreniz :");
+        password=scan.nextLine();
+        if(userName.equals("patika")&&password.equals("java123")){
+            System.out.print("Giris Yaptiniz");
+        }else{
+            System.out.print("Sifre Hatali\nSifrenizi degistirmek icin Evet ise 1, Hayir ise 0 giriniz :  ");
+            int select=scan.nextInt();
+            switch (select){
+                case 1:
+                    System.out.print("Eski Sifreden farkli bir sifre giriniz :");
+                    Scanner input=new Scanner(System.in);
+
+                    String newPasword=input.nextLine();
+                    if(newPasword.equals("java123")){
+                        System.out.println("Lütfen Eskisinden farkli bir sifre giriniz!!!");
+                    }
+                    else
+
+                        System.out.print("Sifre Basarili ile Degistirildi");
+                    break;
+                case 2:
+                    System.out.print("Sifre Degistirilmesi.");
+                    break;
+            }
+        }
+
+
+
+
+
+
+        //hesapMakinasi();
+
+
+    }
+
+    private static void hesapMakinasi() {
         Scanner scan=new Scanner(System.in);
         System.out.print("İlk sayiya giriniz : ");
         int number1=scan.nextInt();
@@ -26,8 +67,5 @@ public class Main {
             default:
                 System.out.print("Hatalı Secim Yaptiniz!!!");
         }
-
-
-
     }
 }
